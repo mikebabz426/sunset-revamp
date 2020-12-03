4ba841ccf94fb4ca134ca21cd20a37150f51eef6
mikebabz426 <60781512+mikebabz426@users.noreply.github.com>
2020-12-03 12:34:48 -0600

    add card component and create services showcase section

---
+++ src/components/Card.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+const CardHolder = styled.div`
+  margin: 2rem auto;
+  max-width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: justify;
+  background-color: #f8f8f8;
+  border-radius: 5px;
+`
+
+const ImageHolder = styled(Img)`
+  width: 320px;
+  height: 300px;
+  border-radius: 5px 5px 0 0;
+`
+const CardHead = styled.h4`
+  font-weight: bold;
+  margin: 1rem 0 0 0;
+`
+
+const Body = styled.p`
+  text-align: justify;
+  max-width: 300px;
+  margin: 1rem 0.5rem;
+`
+const Card = ({ title, fluid }) => {
+  return (
+    <CardHolder>
+      <ImageHolder fluid={fluid}></ImageHolder>
+      <CardHead>{title}</CardHead>
+      <Body>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        provident minus animi voluptatum similique itaque commodi! Minus
+        distinctio consequuntur aspernatur.
+      </Body>
+    </CardHolder>
+  )
+}
+
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,23 @@
 import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 import BackgroundImage from "gatsby-background-image"
 import styled from "styled-components"
 import { graphql } from "gatsby"
 import Button from "./../components/Button"
+import Card from "../components/Card"
 
 const Headline = styled.h1`
   font-weight: bold;
   color: #333;
   font-size: 64px;
   text-align: center;
-  width: 1280px;
+  max-width: 1280px;
+
+  @media (max-width: 600px) {
+    font-size: 32px;
+    max-width: 350px;
+  }
 `
 const BgImage = styled(BackgroundImage)`
   height: 100vh;
@@ -30,18 +36,68 @@ const ButtonBox = styled.div`
   justify-content: space-around;
   align-items: center;
 `
+const ServicesShowcase = styled.div`
+  margin: 0 auto;
+  min-height: 100vh;
+  max-width: 1280;
+`
+const SectionHeadline = styled.h2`
+  color: #333;
+  font-weight: 500;
+  text-align: center;
+  max-width: 1280;
+  margin: 4rem 0 2rem 0;
+  font-size: 32px;
+`
+const SubHeadline = styled.h3`
+  color: #333;
+  font-weight: 500;
+  text-align: center;
+  max-width: 1280;
+  margin: 1rem;
+`
+const CardArea = styled.div`
+  margin: 4rem auto;
+  max-width: 1280px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`
 
 const IndexPage = ({ data }) => {
+  const { hero, offLeash, behavioral, searchRescue } = data
+
   return (
     <Layout>
       <SEO title="Home" />
-      <BgImage className="hero" fluid={data.hero.childImageSharp.fluid}>
+      <BgImage className="hero" fluid={hero.childImageSharp.fluid}>
         <Headline>We Provide Proffessional Training for Your Pets</Headline>
         <ButtonBox>
           <Button type="filled" text="What We Offer" />
           <Button type="outlined" text="Contact Us" />
         </ButtonBox>
       </BgImage>
+      <ServicesShowcase>
+        <SectionHeadline>
+          Get Expert Help Training Your Dog or Puppy
+        </SectionHeadline>
+        <SubHeadline>What we can do for you</SubHeadline>
+        <CardArea>
+          <Card title="Off Leash" fluid={offLeash.childImageSharp.fluid} />
+          <Card
+            title="Search and Rescue"
+            fluid={searchRescue.childImageSharp.fluid}
+          />
+          <Card
+            title="Behavioral Issues"
+            fluid={behavioral.childImageSharp.fluid}
+          />
+        </CardArea>
+      </ServicesShowcase>
     </Layout>
   )
 }
@@ -49,6 +105,27 @@ const IndexPage = ({ data }) => {
 export const query = graphql`
   {
     hero: file(relativePath: { eq: "hero-illu.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1920, pngQuality: 90) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    offLeash: file(relativePath: { eq: "off-leash.png" }) {
+      childImageSharp {
+        fluid(pngQuality: 90) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    behavioral: file(relativePath: { eq: "behavioral.png" }) {
+      childImageSharp {
+        fluid(pngQuality: 90) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    searchRescue: file(relativePath: { eq: "search-rescue.png" }) {
       childImageSharp {
         fluid(pngQuality: 90) {
           ...GatsbyImageSharpFluid
